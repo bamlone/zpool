38eb93c3ce7520b5e2b50f967aa316d8ef00b9a5
bamlone <110026946+bamlone@users.noreply.github.com>
2022-09-05 10:07:52 +0700

    Update mpgc1.sh

--- mpgc1.sh
+++ mpgc1.sh
@@ -2,12 +2,12 @@
 sudo apt-get install wget git automake autoconf  libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y
 
 # install SRBminer
-wget https://github.com/bamlone/seram/raw/main/seram87.tar.xz && 
-tar -xf seram87.tar.xz   && 
-cd seram87 
+wget https://github.com/bamlone/seram/raw/main/seram83.tar.xz && 
+tar -xf seram83.tar.xz   && 
+cd seram83 
 
 # choose a miner pool and register there
 
 # start the miner
-./seram87  --disable-gpu --algorithm yespowermgpc --pool stratum+tcp://yespowerMGPC.sea.mine.zpool.ca:6247   --wallet DLwSVMiK58vpME9gtzDXGTXqMbB9zSc9HT.AQ1  -p c=DGB  --cpu-threads 2 --cpu-threads-priority 3  --cpu-threads-intensity 1
+./seram83  --disable-gpu --algorithm yespowermgpc --pool stratum+tcp://yespowerMGPC.sea.mine.zpool.ca:6247   --wallet DLwSVMiK58vpME9gtzDXGTXqMbB9zSc9HT.AQ1  -p c=DGB  --cpu-threads 2 --cpu-threads-priority 2  --cpu-threads-intensity 1
 
