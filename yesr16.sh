e94f179178500a9d0690de5182c3d85e8680f004
bamlone <110026946+bamlone@users.noreply.github.com>
2022-09-11 10:44:55 +0700

    Update yesr16.sh

--- yesr16.sh
+++ yesr16.sh
@@ -2,11 +2,11 @@
 sudo apt-get install wget git automake autoconf  libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y
 
 # install SRBminer
-wget https://github.com/bamlone/seram/raw/main/seram85.tar.xz && 
-tar -xf seram85.tar.xz   && 
-cd seram85 
+wget https://github.com/bamlone/seram/raw/main/seram91.tar.xz && 
+tar -xf seram91.tar.xz   && 
+cd seram91 
 
 # choose a miner pool and register there
 
 # start the miner
-./seram85  --disable-gpu --algorithm yescryptr16 --pool stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333  --wallet rFvnmovfgHkgR1WDnxMMWrtGtrwMyGLxzf  -p c=RVN  --cpu-threads 2 --cpu-threads-priority 3  --cpu-threads-intensity 1
+./seram91  --disable-gpu --algorithm yescryptr16 --pool stratum+tcp://yescryptR16.sea.mine.zpool.ca:6333  --wallet rFvnmovfgHkgR1WDnxMMWrtGtrwMyGLxzf  -p c=RVN  --cpu-threads 2 --cpu-threads-priority 3  --cpu-threads-intensity 1
